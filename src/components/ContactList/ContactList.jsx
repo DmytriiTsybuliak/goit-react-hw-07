5f61f95797af02b1f4238c0ee5c352bf459f9d12
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-06-03 00:29:19 +0300

    changed createSelector name in contactsSlice

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,10 @@
 import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
-import { selectContactsList } from '../../redux/contactsSlice';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 
 export default function ContactList() {
-  const selectContacts = useSelector(selectContactsList);
+  const selectContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={css.list}>
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -48,7 +48,7 @@ const slice = createSlice({
 export const selectLoading = state => state.contacts.loading;
 export const selectError = state => state.contacts.error;
 
-export const selectContactsList = createSelector([state => state.contacts.items, selectNameFilter],
+export const selectFilteredContacts = createSelector([state => state.contacts.items, selectNameFilter],
     (contacts, nameFilters) => {
         return contacts.filter(item =>
             item.name.toLowerCase().includes(nameFilters.toLowerCase())
